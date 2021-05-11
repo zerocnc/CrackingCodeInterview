1b291967661684dfc4bfd63ce87ef4bdab290b7c
Richard <gdi_richard@yahoo.com>
2021-05-11 15:39:33 -0700

    Finished problem 5

--- python/chap01/problem05.py
+++ python/chap01/problem05.py
@@ -6,11 +6,11 @@ import unittest
 
 def OneEditAway(oldString, newString):
 
-    if len(oldString) == len(newString) + 1:
-        return OneInsert(oldString, newString)
-    elif len(oldString) == len(newString) - 1:
+    if len(oldString) == len(newString):
+        return OneReplace(oldString, newString)
+    elif len(oldString) - 1 == len(newString):
         return OneRemove(oldString, newString)
-    elif len(oldString) == len(newString):
+    elif len(oldString) + 1 == len(newString):
         return OneReplace(oldString, newString)
     else:
         return False
@@ -19,40 +19,61 @@ def OneEditAway(oldString, newString):
 #   Purpose: Checks if one character was replaced.
 def OneReplace(oldString, newString):
 
-    for i in range(0, len(oldString)):
-        if ( oldString[i] != newString[i] ):
-            print( oldString + ", " + newString + " -> " + "false")
-            return False
+    foundDifference = False
 
-    print( oldString + ", " + newString + " -> " + "true" )
+    for i in range(len(oldString)):
+        if(oldString[i] != newString[i]):
+            if(foundDifference):
+                return False
+            foundDifference = True
+
     return True
 
 
 def OneRemove(oldString, newString):
-    pass
+    tempPos_X = 0
+    tempPos_Y = 0
+    flag = True
+
+    for i in range(len(newString)):
+        if oldString[tempPos_X] != newString[tempPos_Y] or flag:
+            continue
+        else:
+            return False
+        
+        tempPos_Y += 1
+
+    return True
 
 def OneInsert(oldString, newString):
-    pass
+    index1 = 0
+    index2 = 0
 
-# My Version Test for all
-def OneAway(originalString, newString):
+    while(index2 < len(newString) and index1 < len(oldString)):
+        if(oldString[index1] != newString[index2]):
+            if (index1 != index2):
+                return False
+            index2 += 1
+        else:
+            index1+=1
+            index2+=1
 
-    if ( len( originalString) == len(newString)):
-        if (OneReplace(originalString, newString)):
-            print("Is replace")
+    return True
 
 class Test(unittest.TestCase):
+    originalString = ["pale", "pales", "pale", "pale"]
+    newString = ["ple", "pale", "bale", "bake"]
 
-    def test_OneEditAway(self):
-        originalString = ["pale", "pales", "pale", "pale"]
-        newString = ["ple", "pale", "bale", "bake"]
+    answerKey = [True, True, True, False]
 
-        answerKey = [True, True, True, False]
+    def test_OneRemove(self):
+        self.assertTrue(OneRemove("pale", "ple"))
 
-        for ndx in range(4):
-            result = OneEditAway(originalString[ndx], newString[ndx])
-            self.assertEqual(result, answerKey[ndx])
-
+    def test_OneInsert(self):
+        self.assertTrue(OneInsert("pale", "pales"))
+    
+    def test_OneReplace(self):
+        self.assertTrue(OneReplace("pale", "bale"))
 
 if __name__ == "__main__":
     unittest.main()
